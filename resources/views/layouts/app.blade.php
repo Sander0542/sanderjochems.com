47d2d128c6c004323c1b78a2f3accd350cfc4549
Sander Jochems <sanderjochems@hotmail.nl>
2022-12-26 12:08:30 +0100

    Add missing meta description tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Personal website of {{ config('personal.about.name') }} containg information about his resume, project and contact information.">
 
     <title>{{ config('app.name') }}</title>
 
